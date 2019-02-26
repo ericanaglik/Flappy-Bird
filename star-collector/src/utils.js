eddeba3a2af5d22d731ea435a9b8f578a278b0a2
erica2605@live.com <erica2605@live.com>
2019-02-25 18:13:08 -0800

    experimented with eventlisteners

---
+++ star-collector/src/utils.js
@@ -0,0 +1,20 @@
+const sayHello = () => {
+    console.log('Hello')
+}
+
+numbers = [33, 12, 74, 8]
+
+const array = () => {
+    for (let number = 0; number < numbers.length; number += 1) {
+        const element = numbers[number];
+    }
+}
+
+const printArray = (arr) => {
+    if (Array.isArray(arr[0])) {
+        // multi array
+    } else {
+
+    }
+}
+
