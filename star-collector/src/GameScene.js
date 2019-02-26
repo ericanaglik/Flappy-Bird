90fc2223db0835a192374879170ce7bdbf25df85
erica2605@live.com <erica2605@live.com>
2019-02-25 20:06:35 -0800

    added platforms

--- star-collector/src/GameScene.js
+++ star-collector/src/GameScene.js
@@ -3,29 +3,29 @@ import { Scene } from 'phaser';
 class GameScene extends Scene {
 
     preload() {
-        this.load.image('logo', 'assets/logo.png');
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 
+            'assets/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
 
     create() {
-        const logo = this.add.image(400, 150, 'logo');
-        const message = this.add.text(50, 100, 'hello');
-        this.input.on('pointerdown', () => {
-            message.text = 'ouch';
-        });
-
-        this.input.on('pointerup', () => {
-            message.text = 'hi'
-        });
-
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: 'Power2',
-            yoyo: true,
-            loop: -1
-        });
+       const sky = this.add.image(0, 0, 'sky');
+       sky.setOrigin(0, 0);
+       this.createPlatforms();
     }
+
+    createPlatforms() {
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+    };
 }
 
 export default GameScene
