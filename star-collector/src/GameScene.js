f845c04b1defa741f1e62e11160ea14202c97035
erica2605@live.com <erica2605@live.com>
2019-03-02 22:56:17 -0800

    fixed some errors but bombs still not working

--- star-collector/src/GameScene.js
+++ star-collector/src/GameScene.js
@@ -148,12 +148,12 @@ class GameScene extends Scene {
     hitBomb (player, bomb) {
         this.physics.pause();
 
-        this.player.setTint(0xff0000);
+        player.setTint(0xff0000);
 
-        this.player.anims.play('turn');
+        player.anims.play('turn');
 
         this.gameOver = true;
-    }
+    };
 }
 export default GameScene
 
