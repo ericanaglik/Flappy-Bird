e17490ba704155d86879168afe1a54c25085e3bc
erica2605@live.com <erica2605@live.com>
2019-03-02 23:58:23 -0800

    started preload scene class

---
+++ star-collector/src/PreloadScene.js
@@ -0,0 +1,16 @@
+import { Scene } from 'phaser'
+
+class PreloadScene extends Scene {
+
+    constructor() {
+        super('preload')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
