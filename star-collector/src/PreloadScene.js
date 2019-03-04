d485588f3bf50e0e46dbf23aa57a6d17845e1a2a
erica2605@live.com <erica2605@live.com>
2019-03-03 16:53:42 -0800

    added particle background to preload screen

--- star-collector/src/PreloadScene.js
+++ star-collector/src/PreloadScene.js
@@ -1,4 +1,4 @@
-import { Scene } from 'phaser';
+import Phaser, { Scene } from 'phaser';
 
 class PreloadScene extends Scene {
   constructor() {
@@ -7,12 +7,22 @@ class PreloadScene extends Scene {
 
   preload() {
     this.load.image('logo', 'assets/logo.png');
+    this.load.image('spark', 'assets/particle.png');
   }
 
   create() {
-    this.add.image(400, 300, 'logo');
+    const logo = this.add.image(400, 300, 'logo');
+    logo.setDepth(99);
     this.input.on('pointerdown', () => this.scene.start('game'));
     this.add.text(100, 500, 'Touch Anywhere to Begin', { fontSize: '40px', fill: '#fff' });
+
+    const particles = this.add.particles('spark');
+
+    const emitter = particles.createEmitter();
+
+    emitter.setPosition(400, 300);
+    emitter.setSpeed(200);
+    emitter.setBlendMode(Phaser.BlendModes.ADD);
   }
 }
 
